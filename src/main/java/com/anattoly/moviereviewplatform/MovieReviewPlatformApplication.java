11a83cf70175046886d69a861d3c264273901bd6
Anatoly Kovalenko <lom.g.lom@gmail.com>
2019-09-18 05:52:43 +0300

    delete prop

--- src/main/java/com/anattoly/moviereviewplatform/MovieReviewPlatformApplication.java
+++ src/main/java/com/anattoly/moviereviewplatform/MovieReviewPlatformApplication.java
@@ -8,7 +8,6 @@ import org.telegram.telegrambots.ApiContextInitializer;
 public class MovieReviewPlatformApplication {
 
     public static void main(String[] args) {
-        ApiContextInitializer.init();
         SpringApplication.run(MovieReviewPlatformApplication.class, args);
     }
 
